a444f76486f378b07c885b1b2a8d3131b583621e
TurtleWolfe <JonPohlner@gmail.com>
2021-01-10 18:37:49 -0500

    first issue

--- dist/javaScript/javaScript.js
+++ dist/javaScript/javaScript.js
@@ -1,2 +1,14 @@
 console.log('this works');
 console.log(d3);
+
+var WIDTH = 800;
+var HEIGHT = 600;
+
+d3.select('svg')
+  .style("fill", "red")
+  .style('width', WIDTH)
+  .style('height', HEIGHT);
+
+d3.select("main")
+  .append("h1")
+  .text("Very important item");
